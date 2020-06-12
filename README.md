29339200bb888625f5c67c7e906b87fb706ac8b0
Vijay Marupudi <vijay@vijaymarupudi.com>
2020-06-12 12:01:27 -0500

    Incremental progress on simplying the library

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # reaction-time-js
 
 A javascript / typescript library for online psychology experiments.
+
+## What is a timeline generator?
+
+A timeline generator is an iterable that yields plugins.
+
+## What is a plugin?
+
+```typescript
+function plugin(screen: HTMLElement);
+```
+
+More specifically, a plugin is a closure function that contains the
+configuration for the experiment. The core of reaction-time-js has no idea of
+what the configuration provided to the plugin was.
+
+It is responsibility of the plugin to include the configuration in the final
+data object it returns, along with identifying information about which plugin
+was run.
--- src/reaction-time.ts
+++ src/reaction-time.ts
@@ -5,11 +5,21 @@ import { Stopwatch } from "./Stopwatch";
 
 type TimelineGenerator = { (reactionTime: ReactionTime): Iterable<ITrial> };
 
+function displayData(data: Object, screen: HTMLElement): void {
+  const pre = document.createElement('pre')
+  const code = document.createElement('code')
+  code.innerText = JSON.stringify(data, null, 2);
+  pre.appendChild(code)
+
+  screen.innerHTML = ''
+  screen.appendChild(pre)
+}
+
 export async function init(timelineGenerator: TimelineGenerator) {
   const screen = document.createElement("div");
   document.body.appendChild(screen);
-  const reactionTime = new ReactionTime(screen);
-  const timeline = timelineGenerator(reactionTime)[Symbol.iterator]();
+  const data = []
+  const timeline = timelineGenerator(data)[Symbol.iterator]();
   let previousTrialData: any;
   const stopwatch = new Stopwatch();
   let trialIndex = 0;
@@ -25,11 +35,13 @@ export async function init(timelineGenerator: TimelineGenerator) {
       trialIndex,
       ...pluginTrialData
     };
-    reactionTime.push(finalTrialData);
+    data.push(finalTrialData);
     trialIndex++;
     // wrapping up for next iteration
     previousTrialData = finalTrialData;
   }
+
+  displayData(data, screen);
 }
 
 export { jsPsychPlugin };
