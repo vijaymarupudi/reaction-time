f2c196e43cc4556ecb3287bda0b9490d3cd35cd8
Vijay Marupudi <vijay@vijaymarupudi.com>
2020-06-18 18:45:29 -0500

    Added css to combined build

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,10 @@ module.exports = [
       rules: [
         babelfilter,
         {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"]
+        },
+        {
           test: /\.tsx?$/,
           use: "ts-loader",
           exclude: /node_modules/
