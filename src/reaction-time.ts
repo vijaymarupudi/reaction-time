15046fd459c781c0d3457ab9a8e927439d8447b8
Vijay Marupudi <vijay@vijaymarupudi.com>
2020-06-19 15:20:37 -0500

    Fixed css centering issues

--- src/reaction-time.ts
+++ src/reaction-time.ts
@@ -29,18 +29,21 @@ export async function init(timelineGenerator: ITimelineGenerator) {
 
   targetElement.innerHTML = '';
 
+  // not using min-height because of https://stackoverflow.com/questions/8468066/child-inside-parent-with-min-height-100-not-inheriting-height
+
   // screenContainer takes the dimensions of the targetElement
   const screenContainer = document.createElement('div')
   targetElement.appendChild(screenContainer);
   screenContainer.style.width = '100%'
   screenContainer.style.height = '100%'
   screenContainer.style.display = 'flex';
-  screenContainer.style.alignItems = 'center';
-  screenContainer.style.justifyContent = 'center';
 
   
-  // screen is centered within the screen container.
   const screen = document.createElement('div')
+  // screen is centered within the screen container.
+  // this method is for IE compat and for oversized elements to be scrollable
+  // https://stackoverflow.com/questions/33454533/cant-scroll-to-top-of-flex-item-that-is-overflowing-container
+  screen.style.margin = 'auto'
   screenContainer.appendChild(screen)
 
   // Array that holds all the data
