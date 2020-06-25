994f77920db0a78f9f3e9bdaf5803e12ecbd3721
Vijay Marupudi <vijay@vijaymarupudi.com>
2020-06-25 08:09:32 -0500

    Removed React and simplified build

--- rollup.config.js
+++ rollup.config.js
@@ -1,70 +1,87 @@
-import { terser } from "rollup-plugin-terser";
-import { babel } from "@rollup/plugin-babel";
-import commonjs from "@rollup/plugin-commonjs";
-import resolve from "@rollup/plugin-node-resolve";
+// import { terser } from "rollup-plugin-terser";
+// import { babel } from "@rollup/plugin-babel";
+// import commonjs from "@rollup/plugin-commonjs";
+// import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 
-export default [
-  {
-    input: "./src/reaction-time.ts",
-    plugins: [
-      typescript({
-        jsx: 'react'
-      }),
-      babel({
-        babelHelpers: "bundled",
-        presets: [
-          [
-            "@babel/preset-env",
-            {
-              targets: "> 0.25%, not dead",
-              useBuiltIns: "usage",
-              corejs: 3
-            }
-          ]
-        ],
-        exclude: "node_modules/**/*",
-        extensions: [".js", ".ts", ".tsx"]
-      }),
-      resolve(),
-      commonjs()
-    ],
+function esmBasicBuild(inputFile, outDir) {
+  return {
+    input: inputFile,
+    plugins:[ typescript(
+      { declarationDir: 'dist/' + outDir }
+    ) ],
     output: {
-      file: "dist/reaction-time.umd.js",
-      format: "umd",
-      name: "ReactionTime",
-      plugins: [terser()]
-    }
-  },
-  {
-    input: "./src/reaction-time-jspsych.ts",
-    plugins: [
-      typescript({
-        jsx: 'react'
-      }),
-      babel({
-        babelHelpers: "bundled",
-        presets: [
-          [
-            "@babel/preset-env",
-            {
-              targets: "> 0.25%, not dead",
-              useBuiltIns: "usage",
-              corejs: 3
-            }
-          ]
-        ],
-        exclude: "node_modules/**/*",
-        extensions: [".js", ".ts", ".tsx"]
-      }),
-      resolve(),
-      commonjs()
-    ],
-    output: {
-      file: "dist/reaction-time-jspsych.umd.js",
-      format: "umd",
-      name: "ReactionTime",
-      plugins: [terser()]
+      dir: 'dist/' + outDir,
+      format: 'es'
     }
   }
+}
+
+export default [
+  esmBasicBuild('./src/reaction-time-jspsych.ts', 'reaction-time-jspsych'),
+  esmBasicBuild('./src/reaction-time.ts', 'reaction-time')
+  // {
+  //   input: "./src/reaction-time.ts",
+  //   plugins: [
+  //     typescript({
+  //       jsx: 'react'
+  //     }),
+  //     babel({
+  //       babelHelpers: "bundled",
+  //       presets: [
+  //         [
+  //           "@babel/preset-env",
+  //           {
+  //             targets: "> 0.25%, not dead",
+  //             useBuiltIns: "usage",
+  //             corejs: 3
+  //           }
+  //         ]
+  //       ],
+  //       exclude: "node_modules/**/*",
+  //       extensions: [".js", ".ts", ".tsx"]
+  //     }),
+  //     resolve(),
+  //     commonjs()
+  //   ],
+  //   output: {
+  //     file: "dist/reaction-time.umd.js",
+  //     format: "umd",
+  //     name: "ReactionTime",
+  //     plugins: [terser()]
+  //   }
+  // },
+  // {
+    // input: "./src/reaction-time-jspsych.ts",
+    // plugins: [
+    //   typescript({
+    //     jsx: 'react'
+    //   }),
+    //   babel({
+    //     babelHelpers: "bundled",
+    //     presets: [
+    //       [
+    //         "@babel/preset-env",
+    //         {
+    //           targets: "> 0.25%, not dead",
+    //           useBuiltIns: "usage",
+    //           corejs: 3
+    //         }
+    //       ]
+    //     ],
+    //     exclude: "node_modules/**/*",
+    //     extensions: [".js", ".ts", ".tsx"]
+    //   }),
+    //   resolve(),
+    //   commonjs()
+    // ],
+    // output: {
+    //   file: "dist/reaction-time-jspsych.umd.js",
+    //   format: "umd",
+    //   name: "ReactionTime",
+    //   plugins: [terser()]
+    // }
+  // },
+
+
 ];
--- src/react-plugin-types.ts
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-
-export type IReactPluginComponent<T> = React.FunctionComponent<{
-  config: T;
-  finishTrial: { (trialData: Object): void };
-}>;
-
-
