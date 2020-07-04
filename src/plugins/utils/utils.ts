72e87135559e3097cfc73fe6fb863d7b65beed60
Vijay Marupudi <vijay@vijaymarupudi.com>
2020-07-04 10:59:44 -0500

    Added Stopwatch utility

--- src/plugins/utils/utils.ts
+++ src/plugins/utils/utils.ts
@@ -1,4 +1,27 @@
-import cxs from 'cxs'
-import m from './m'
+import cxs from "cxs";
+import m from "./m";
+import afterframe from "afterframe";
 
-export { cxs, m }
+export class Stopwatch {
+  private startDT: number = null;
+  private startTS: number = null;
+  start(): void {
+    afterframe(() => {
+      this.startDT = new Date().valueOf();
+      this.startTS = performance.now();
+    });
+  }
+
+  stop(): { startDateTime: number; stopDateTime: number; duration: number } {
+    return {
+      startDateTime: this.startDT,
+      stopDateTime: new Date().valueOf(),
+      duration: performance.now() - this.startTS,
+    };
+  }
+}
+
+export const wait = (ms: number): Promise<void> =>
+  new Promise((resolve) => setTimeout(resolve, ms));
+
+export { cxs, m };
