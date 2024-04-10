3669635f1b64bb782c8d95fb8089686b89399cba
Vijay Marupudi <vijay@vijaymarupudi.com>
2024-04-10 17:25:47 -0400

    Dependency cleanup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,4 +1,4 @@
-import { terser } from "rollup-plugin-terser";
+import  terser  from "@rollup/plugin-terser";
 import { babel } from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
